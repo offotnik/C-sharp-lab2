6ea95192410fc91188d48af609e86c907ae452e4
Artem <offotnik198@gmail.com>
2025-12-24 12:36:06 +0500

    Update Names.cs

--- LABPIR2/LABPIR2/Names.cs
+++ LABPIR2/LABPIR2/Names.cs
@@ -4,51 +4,33 @@ namespace Lab2
 {
     internal class Names
     {
-        private string name;
-        private string last_name;
-        private string middle_name;
+        private string _name;
+        private string _last_name;
+        private string _middle_name;
 
         public string Name
         {
-            get 
-            { 
-                return name; 
-            }
-            set 
-            { 
-                name = value; 
-            }
+            get { return _name; }
+            set { _name = value; }
         }
 
         public string Last_Name
         {
-            get 
-            { 
-                return last_name; 
-            }
-            set 
-            { 
-                last_name = value; 
-            }
+            get { return _last_name; }
+            set { _last_name = value; }
         }
 
         public string Middle_Name
         {
-            get 
-            { 
-                return middle_name; 
-            }
-            set 
-            { 
-                middle_name = value; 
-            }
+            get { return _middle_name; }
+            set { _middle_name = value; }
         }
 
         public Names(string name, string last_name, string middle_name)
         {
-            this.name = name;
-            this.last_name = last_name;
-            this.middle_name = middle_name;
+            _name = name;
+            _last_name = last_name;
+            _middle_name = middle_name;
         }
 
         public override string ToString()
@@ -59,9 +41,9 @@ namespace Lab2
 
         private int DetermineSituation()
         {
-            bool nameIsEmpty = string.IsNullOrWhiteSpace(name);
-            bool lastNameIsEmpty = string.IsNullOrWhiteSpace(last_name);
-            bool middleNameIsEmpty = string.IsNullOrWhiteSpace(middle_name);
+            bool nameIsEmpty = string.IsNullOrWhiteSpace(_name);
+            bool lastNameIsEmpty = string.IsNullOrWhiteSpace(_last_name);
+            bool middleNameIsEmpty = string.IsNullOrWhiteSpace(_middle_name);
 
             if (nameIsEmpty && lastNameIsEmpty && middleNameIsEmpty)
             {
@@ -106,19 +88,19 @@ namespace Lab2
             switch (situation)
             {
                 case 0:
-                    return $"{last_name} {name} {middle_name}";
+                    return $"{_last_name} {_name} {_middle_name}";
                 case 1:
-                    return $"{last_name} {middle_name}";
+                    return $"{_last_name} {_middle_name}";
                 case 2:
-                    return $"{name} {middle_name}";
+                    return $"{_name} {_middle_name}";
                 case 3:
-                    return $"{last_name} {name}";
+                    return $"{_last_name} {_name}";
                 case 4:
-                    return name;
+                    return _name;
                 case 5:
-                    return middle_name;
+                    return _middle_name;
                 case 6:
-                    return last_name;
+                    return _last_name;
                 case 7:
                     return "Вы ничего не написали";
                 default:
